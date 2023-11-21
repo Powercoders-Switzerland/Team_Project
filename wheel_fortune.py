9f005c3e79f33aa2f3e2127bfedba0407b01a3ad
YidnekG168 <yidnek168@gmail.com>
2023-11-21 16:10:39 +0100

    WOF ComputerPlayer with HumanPlayer

--- wheel_fortune.py
+++ wheel_fortune.py
@@ -12,3 +12,51 @@ class WOFPlayer():
         self.prizes.append(prize)
     def __str__(self):
         return '{} (${})'.format(self.name, self.prizeMoney)
+    
+    class WOFHumanPlayer(WOFPlayer):
+        
+    def getMove(self, category, obscuredPhrase, guessed):
+        print("""{} has ${}\n Category: {}\n Phrase:  {} \n
+        Guessed: {}""".format(self.name, self.prizeMoney, category, obscuredPhrase, guessed))
+        return input("Guess a letter, phrase, or type 'exit' or 'pass':")
+        
+# WOFComputerPlayer class definition 
+class WOFComputerPlayer(WOFPlayer):
+             
+    SORTED_FREQUENCIES = 'ZQXJKVBPYGFWMUCLDRHSNIOATE'
+    def __init__(self, name, difficulty):
+        WOFPlayer.__init__(self, name)
+        self.difficulty = difficulty 
+    def smartCoinFlip(self):
+        if (random.randint(1, 10) > self.difficulty):
+             return False
+        else:
+             return True
+             
+    def getPossibleLetters(self, guessed):
+        lst_letters = []
+        for LETTER in LETTERS:
+             if LETTER not in guessed:
+                if LETTER in VOWELS:
+                     if self.prizeMoney >= VOWEL_COST:
+                        lst_letters.append(LETTER)
+                else:         
+                     lst_letters.append(LETTER)
+        return lst_letters
+             
+    def getMove(self, category, obscuredPhrase, guessed): 
+        r_SORTED_FREQUENCIES_lst = list(self.SORTED_FREQUENCIES)
+        r_SORTED_FREQUENCIES_lst.reverse()
+        i = 0
+        for VOWEL in VOWELS:
+             if VOWEL in self.getPossibleLetters(guessed):
+                i += 1
+        if i == 0:
+             return 'pass'
+        if self.smartCoinFlip():
+            for L in r_SORTED_FREQUENCIES_lst:
+                if L in self.getPossibleLetters(guessed):
+                     return L
+        else:
+             return random.choice(self.getPossibleLetters(guessed))
+         
